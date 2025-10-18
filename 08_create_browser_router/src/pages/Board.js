b9bb60d2808848b8ffef37b3cb9926b5748727a5
109kms <109kms@naver.com>
2025-10-18 19:18:22 +0900

    251018 createBrowserRouter 공부

--- 08_create_browser_router/src/pages/Board.js
+++ 08_create_browser_router/src/pages/Board.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Board = () => {
   return (
     <div>
       <h1>Board Page</h1>
       <p>게시판 페이지입니다.</p>
+      <Link to="/">홈으로 이동</Link>
     </div>
   );
 };
