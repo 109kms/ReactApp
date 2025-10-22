b5e4fcca117f378c6e4365784eefdb81a8628093
109kms <109kms@naver.com>
2025-10-22 12:46:20 +0900

    251022

--- 16_user/user_boot/src/main/java/org/shark/appuser/user/config/AppConfig.java
+++ 16_user/user_boot/src/main/java/org/shark/appuser/user/config/AppConfig.java
@@ -14,4 +14,27 @@ public class AppConfig {
     return new BCryptPasswordEncoder();
   }
   
+  /* JwtUserDetailsService 클래스를 대체할 수 있는 빈 */
+  //----- 1. 익명 객체 활용
+  /*
+  @Bean
+  UserDetailsService userDetailsService() {
+    return new UserDetailsService() {
+      @Override
+      public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+            .orElseThrow(() -> new UsernameNotFoundException(username + " 사용자를 찾을 수 없습니다."));
+      }
+    };
+  }
+  */
+  // 2. 람다식 활용
+  /*
+  @Bean
+  UserDetailsService userDetailsService() {
+    return username -> userRepository.findByEmail(username)
+                            .orElseThrow(() -> new UsernameNotFoundException(username + " 사용자를 찾을 수 없습니다."));
+  }
+  */
+  
 }
--- 16_user/user_boot/src/test/java/org/shark/appuser/UserBootApplicationTests.java
+++ 16_user/user_boot/src/test/java/org/shark/appuser/UserBootApplicationTests.java
@@ -1,13 +1,27 @@
 package org.shark.appuser;
 
+import java.security.Key;
+import java.util.Base64;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import io.jsonwebtoken.Jwts;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootTest
 class UserBootApplicationTests {
 
+  // JWT 토큰 생성을 위해서는 서명키가 필요합니다.
+  // 서명키(SecretKey) 생성을 위해서는 Base64 인코딩 방식의 비밀키(문자열)가 필요합니다.
+  
+  // Base64 인코딩된 비밀키 만들기
 	@Test
-	void contextLoads() {
+	void generateBase64EncodedKey() {
+	  Key key = Jwts.SIG.HS256.key().build();
+	  String encodedKey = Base64.getEncoder().encodeToString(key.getEncoded());
+	  log.info("Base64 Encoded Key:{}", encodedKey);
 	}
 
 }
